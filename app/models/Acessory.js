b4511560936eeb9d789653754e70ff8ce9590837
NexaprimeGit <tech@nexaprime.int>
2026-04-21 12:34:13 +0530

    changing models by commenting code

--- app/models/Acessory.js
+++ app/models/Acessory.js
@@ -1,25 +1,25 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const accessorySchema = new mongoose.Schema(
-  {
-    title: String,
-    slug: { type: String, unique: true },
-    image: String,
-    description: String,
+// const accessorySchema = new mongoose.Schema(
+//   {
+//     title: String,
+//     slug: { type: String, unique: true },
+//     image: String,
+//     description: String,
 
-    specifications: [
-      {
-        label: String,
-        value: String,
-        _id: false, // 🔥 IMPORTANT
-      },
-    ],
+//     specifications: [
+//       {
+//         label: String,
+//         value: String,
+//         _id: false, // 🔥 IMPORTANT
+//       },
+//     ],
 
-    price: Number,
-    isActive: { type: Boolean, default: true },
-  },
-  { timestamps: true },
-);
+//     price: Number,
+//     isActive: { type: Boolean, default: true },
+//   },
+//   { timestamps: true },
+// );
 
-export default mongoose.models.Accessory ||
-  mongoose.model('Accessory', accessorySchema);
+// export default mongoose.models.Accessory ||
+//   mongoose.model('Accessory', accessorySchema);
